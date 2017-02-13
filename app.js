7fca75696a932531ddd13640911b60f9f7ae4b00
PradeepJaiswar <mado023@gmail.com>
2017-02-13 16:40:25 +0530

    read .json from file

--- app.js
+++ app.js
@@ -3,27 +3,15 @@ var app = express();
 var cors = require('cors');
 app.use(cors());
 app.options('*', cors());
-
-var response = {
-  "id": 41876,
-  "first_name": "PradeepJaiswar",
-  "last_name": null,
-  "full_name": "PradeepJaiswar",
-  "username": "PradeepJaiswar",
-  "email": "mado023@gmail.com",
-  "github_gravatar": "https://avatars.githubusercontent.com/u/4545996",
-  "github_uid": 4545996,
-  "learn_verified_user": false,
-  "can_start_working": true,
-};
+var userData = require('./user.json');
 
 app.get('/', function (req, res) {
-  res.send(response);
+  res.send(userData);
   res.end();
 });
 
 app.get('/api/v1/users/me', function (req, res) {
-  res.send(response);
+  res.send(userData);
   res.end();
 });
 
